6892876957e0b29edf017d82f32c7a8c13cde348
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-19 01:55:43 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,10 @@
 def my_each(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 0
-  end
-  array
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 0
+    end  
+    array
+  else puts "This block should not run!"
 end
